36c487241bd92e993f00de16438ca3595a17531b
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-23 16:06:12 +0530

    implemented tic tack toe layout in react home component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -6,10 +6,28 @@ function Home() {
       <figure class="bg-slate-100 rounded-xl py-6  dark:bg-red-300 w-96 mx-auto mt-7 shadow-xl">
         <h1 className="text-3xl font-bold text-center text-dark-50 ">
           {" "}
-          <span className="p-2 bg-slate-50 rounded-full me-2"> 🐒</span> Tic Tack Toe{" "}
-          <span className="p-2 bg-slate-50 rounded-full ms-2">🐸</span>{" "}
+          <span className="p-2 bg-slate-50 rounded-full me-2"> 🐒</span> Tic
+          Tack Toe <span className="p-2 bg-slate-50 rounded-full ms-2">🐸</span>{" "}
         </h1>
       </figure>
+      {/* game box  */}
+      <div className="mt-20 bord w-fit block mx-auto p-5 rounded-md shadow-3xl bg-red-300 ">
+        <div className="row flex justify-center">
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+        </div>
+        <div className="row flex justify-center">
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+        </div>
+        <div className="row flex justify-center">
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out hover:bg-white cursor-grab	 "></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+          <div className="box w-28 h-28 bg-slate-100 border border-red-400 hover:shadow-sau hover:z-50 transition delay-150 duration-100 ease-in-out  hover:bg-white cursor-grab	"></div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        sau: "0px 0px 10px rgba(0, 0, 0, 0.4)",
+        "3xl":
+          "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset",
+      },
+    },
   },
   plugins: [],
-}
+};
+  
